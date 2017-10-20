24886acc6a029bb17f4a10d3e7e80ddfb501b446
root <root@ts.tskshy.io>
2017-10-20 18:05:38 +0800

    update: Fri Oct 20 18:05:37 CST 2017

--- src/libs/log/log_test.go
+++ src/libs/log/log_test.go
@@ -1,9 +1,44 @@
 package log
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
+type I interface {
+	Say()
+}
+
+type Person struct {
+	Name string
+}
+
+func (p *Person) Say() {
+	fmt.Println(p.Name)
+}
+
+func (p *Person) Writer(pp []byte) (int, error) {
+	fmt.Println(p.Name)
+	return len(pp), nil
+}
+
 func Test_Log(t *testing.T) {
-	Debug("hello")
+	var p = &Person{
+		Name: "abc",
+	}
+
+	var is I = p
+	is.Say()
+
+	var s = os.Stdout
+	fmt.Println(s.Fd(), s.Name())
+	//s = os.Stderr
+	//fmt.Println(s.Fd(), s.Name())
+	//s = os.Stdin
+	//fmt.Println(s.Fd(), s.Name())
+	s, _ = os.OpenFile("app.log1", os.O_CREATE|os.O_RDWR, 0666)
+	fmt.Println(s.Fd(), s.Name())
+	s, _ = os.OpenFile("app.log", os.O_CREATE|os.O_RDWR, 0666)
+	fmt.Println(s.Fd(), s.Name())
 }
